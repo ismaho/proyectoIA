618b7ebcb6ed45a7e7c4b2a3a797d4795daccf29
Mahonri Martinez <imahonri@gmail.com>
2020-12-03 21:31:15 -0600

    codigo usando PCA

--- train_2.py
+++ train_2.py
@@ -9,6 +9,8 @@ from  sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -24,15 +26,58 @@ def get_data(din):
     return bf
 
 
-def get_score_threshold(X, y):
+def get_score_threshold(X_digits, y_digits):
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    reg = LogisticRegression()
-    y_log = reg.fit(X_train, y_train)
-    prediction = y_log.predict(X_test)
 
+    # Define a pipeline to search for the best combination of PCA truncation
+    # and classifier regularization.
+    pca = PCA()
+    # set the tolerance to a large value to make the example faster
+    logistic = LogisticRegression(max_iter=10000, tol=0.1)
+    pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
+
+    #X_digits, y_digits = datasets.load_digits(return_X_y=True)
+
+    # Parameters of pipelines can be set using ‘__’ separated parameter names:
+    param_grid = {
+        'pca__n_components': [32, 64, 128 ],
+        'logistic__C': np.logspace(-4, 4, 4),
+    }
+    search = GridSearchCV(pipe, param_grid, n_jobs=-1)
+    search.fit(X_digits, y_digits)
+    print("Best parameter (CV score=%0.3f):" % search.best_score_)
+    print(search.best_params_)
+
+    
+    # Plot the PCA spectrum
+    pca.fit(X_digits)
+
+    fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True, figsize=(6, 6))
+    ax0.plot(np.arange(1, pca.n_components_ + 1),
+            pca.explained_variance_ratio_, '+', linewidth=2)
+    ax0.set_ylabel('PCA explained variance ratio')
+
+    ax0.axvline(search.best_estimator_.named_steps['pca'].n_components,
+                linestyle=':', label='n_components chosen')
+    ax0.legend(prop=dict(size=12))
+
+    # For each number of components, find the best classifier results
+    results = pd.DataFrame(search.cv_results_)
+    components_col = 'param_pca__n_components'
+    best_clfs = results.groupby(components_col).apply(
+        lambda g: g.nlargest(1, 'mean_test_score'))
+
+    best_clfs.plot(x=components_col, y='mean_test_score', yerr='std_test_score',
+                legend=False, ax=ax1)
+    ax1.set_ylabel('Classification accuracy (val)')
+    ax1.set_xlabel('n_components')
+
+    plt.xlim(-1, 70)
+
+    plt.tight_layout()
+    plt.show()
  
-    return prediction, X_test, y_test, reg
+    return True
 
 
 
@@ -55,7 +100,7 @@ for opt in opts:
         y_test = np.zeros(len(X_test), dtype=int)
         y_test[len(normal2):] = 1
         
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
 
         y_pred, X_test, y_test, modelo = get_score_threshold(X_test,y_test)
 
