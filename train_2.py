bfae5da4fd265845eca142a63c80e9c89f0b230c
ismaho <imahonri@gmail.com>
2020-12-07 22:10:47 -0600

    Se Añadio matriz de confusión y se corrigió el error y lee todos los archivos

--- train_2.py
+++ train_2.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 import matplotlib.pyplot as plt
-from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from  sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
@@ -11,6 +10,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
 
 
 
@@ -24,7 +24,6 @@ def get_data(din):
     bf = bf[cols]
     bf = bf.transpose()
     bf.columns = genes
-    #bf = pd.get_dummies(bf)
     return bf
 
 
@@ -34,7 +33,7 @@ def get_score_threshold(X, y):
     y_digits = y
     pca = PCA()
     # set the tolerance to a large value to make the example faster
-    logistic = LogisticRegression(max_iter=10000, tol=0.1)
+    logistic = LogisticRegression(max_iter=100000, tol=0.1)
     pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
 
     # Parameters of pipelines can be set using ‘__’ separated parameter names:
@@ -89,22 +88,14 @@ result = []
 
 
 for opt in opts:
-    din1 = dirin + opt[0] + '-rsem-fpkm-gtex.txt'
     din2 = dirin + opt[1] + '-rsem-fpkm-tcga.txt'
     din3 = dirin + opt[1] + '-rsem-fpkm-tcga-t.txt'
-    if os.path.isfile(din1) and os.path.isfile(din2) and os.path.isfile(din3):
-        normal1 = get_data(din1)
+    if os.path.isfile(din2) and os.path.isfile(din3):
         normal2 = get_data(din2)
         abnormal = get_data(din3)       
         X_test = pd.concat( [normal2, abnormal] ).values
         y_test = np.zeros(len(X_test), dtype=int)
         y_test[len(normal2):] = 1
-
-        print(X_test)
-        print(X_test.shape)
-        print(y_test)
-        print(y_test.shape)
-        
         #import pdb; pdb.set_trace()
 
     
@@ -113,19 +104,24 @@ for opt in opts:
         Accuracy = score
         tn, fp, fn, tp = metrics.confusion_matrix(y_test, y_pred).ravel()
         Specificity = tn / (tn+fp)
-        
+
+        title = opt[0] + " Confusion matrix"  
+        disp = plot_confusion_matrix(model, X_test, y_test, cmap= plt.cm.Blues, normalize = 'true', display_labels = [("No tumor"),("Tumor")] )
+        disp.ax_.set_title(title)
+        plt.show()
+
         ret = [Accuracy]
         ret = [round(e,3) for e in ret]
-        ret = [opt[0], len(normal1), len(normal2), len(abnormal)]  + ret
+        ret = [opt[0], len(normal2), len(abnormal)]  + ret
         print(ret[0],ret[-6:])     
         result += [ ret ]
 
         data_idx = np.arange(len(X_test))
         plt.subplots(figsize=(10, 10))
-        plt.scatter(data_idx[y_test==1], y_test[y_test==1],s= 10, label='Real Tumor', color='black')
-        plt.scatter(data_idx[y_test==1], y_pred[y_test==1], s= 10, color='red',label='Predicción tumor')
-        plt.scatter(data_idx[y_test==0], y_test[y_test==0],s= 10, label='Real Normal', color='blue')
-        plt.scatter(data_idx[y_test==0], y_pred[y_test==0], s= 10, color='green',label='Predicción normal')
+        plt.scatter(data_idx[y_test==1], y_test[y_test == 1],s= 20, label='Real Tumor', color='black')
+        plt.scatter(data_idx[y_test==1], y_pred[y_test >= 1], s= 10, color='red',label='Predicción tumor')
+        plt.scatter(data_idx[y_test==0], y_test[y_test == 0],s= 20, label='Real Normal', color='blue')
+        plt.scatter(data_idx[y_test==0], y_pred[y_test <= 0], s= 10, color='green',label='Predicción normal')
         plt.xlabel('Data point',fontsize=18)
         plt.ylabel('Residual signal',fontsize=18)
         plt.title(opt[0],fontsize=20)
